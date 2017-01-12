a876c38504ff5c27c7b17ec0cc993d4c290ca35f
Jan Posz <janek@vormlab.com>
2017-01-12 23:21:58 +0100

    Added global namespace

--- MersenneRandom/main.cpp
+++ MersenneRandom/main.cpp
@@ -13,14 +13,16 @@
 #include <string>
 #include <random>
 
+using namespace std;
+
 static int alfabet_length[5] = {5,10,15,20,26};
-static std::string alfabet_1[5] = {"a","b","c","d","e"};
-__unused std::string alfabet_2[10] = {"a","b","c","d","e","f","g","h","i","j"};
-static std::string alfabet_3[15] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"};
-static std::string alfabet_4[20] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"};
-static std::string alfabet_5[26] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","x","z"};
+static string alfabet_1[5] = {"a","b","c","d","e"};
+__unused string alfabet_2[10] = {"a","b","c","d","e","f","g","h","i","j"};
+static string alfabet_3[15] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"};
+static string alfabet_4[20] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"};
+static string alfabet_5[26] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","x","z"};
 int times = 50;
-static std::string *tab[5] = {alfabet_1,alfabet_4,alfabet_3,alfabet_4,alfabet_5};
+static string *tab[5] = {alfabet_1,alfabet_4,alfabet_3,alfabet_4,alfabet_5};
 
 void generateMersenneFiles() {
     
@@ -30,15 +32,15 @@ void generateMersenneFiles() {
     MersenneGeneratorConfiguration *configuration = new MersenneGeneratorConfiguration();
     MersenneGenerator *generator = new MersenneGenerator(configuration, seed);
     
-    std::fstream myfile;
+    fstream myfile;
     
     for(int j=0 ;j<5;j++) {
-        myfile.open(files[j], std::fstream::binary | std::fstream::in | std::fstream::out | std::fstream::trunc);
+        myfile.open(files[j], fstream::binary | fstream::in | fstream::out | fstream::trunc);
         myfile.flush();
         for(int i=0 ;i<times;i++) {
-            std::string rand = tab[j][generator->mersenneRandomGeneratedNumberWithUpperBound(alfabet_length[j])];
-            std::cout << &myfile;
-            myfile << rand << std::endl;
+            string rand = tab[j][generator->mersenneRandomGeneratedNumberWithUpperBound(alfabet_length[j])];
+            cout << &myfile;
+            myfile << rand << endl;
         }
         myfile.close();
     }
@@ -53,17 +55,17 @@ void generateMinSTDRandom() {
     
     const char* files[5] = {"/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/minSTD_1.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/minSTD_2.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/minSTD_3.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/minSTD_4.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/minSTD_5.txt"};
     
-    std::fstream myfile;
+    fstream myfile;
     
     for(int j=0 ;j<5;j++) {
-        myfile.open(files[j], std::fstream::binary | std::fstream::in | std::fstream::out | std::fstream::trunc);
+        myfile.open(files[j], fstream::binary | fstream::in | fstream::out | fstream::trunc);
         myfile.flush();
-        std::minstd_rand gen(seed);
-        std::uniform_int_distribution<> distr(0, alfabet_length[j]);
+        minstd_rand gen(seed);
+        uniform_int_distribution<> distr(0, alfabet_length[j]);
         for(int i=0 ;i<times;i++) {
-            std::string rand = tab[j][distr(gen)];
-            std::cout << rand << std::endl;
-            myfile << rand << std::endl;
+            string rand = tab[j][distr(gen)];
+            cout << rand << endl;
+            myfile << rand << endl;
         }
         myfile.close();
     }
@@ -77,15 +79,15 @@ void generateKnuthRandom() {
     
     const char* files[5] = {"/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/knuth_1.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/knuth_2.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/knuth_3.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/knuth_4.txt","/Users/Janek/Documents/Studia/TiK/MersenneRandomGenerator/MersenneRandom/MersenneRandom/knuth_5.txt"};
     
-    std::knuth_b knuth(seed);
-    std::fstream myfile;
+    knuth_b knuth(seed);
+    fstream myfile;
     
     for(int j=0 ;j<5;j++) {
-        myfile.open(files[j], std::fstream::binary | std::fstream::in | std::fstream::out | std::fstream::trunc);
+        myfile.open(files[j], fstream::binary | fstream::in | fstream::out | fstream::trunc);
         int alphabetLength = alfabet_length[j];
         for(int i=0 ;i<times;i++) {
-            std::string rand = tab[j][knuth() % alphabetLength];
-            myfile << rand << std::endl;
+            string rand = tab[j][knuth() % alphabetLength];
+            myfile << rand << endl;
         }
         myfile.close();
     }
